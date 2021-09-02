f10b7753cc09159daf069f90580f898572c62aa4
PavelRoR <pavelvp2182@yandex.ru>
2021-09-02 19:53:14 +0500

    Main and Promo edits. Alexander added

--- js/scripts.js
+++ js/scripts.js
@@ -2,52 +2,28 @@ $(document).ready(function () {
     $(function () {
         var check = $('.check', this),
             email = $('.input-mail', this),
-            message = $('.alert-message', this),
-            button = $('.button-form', this);
+            message = $('.alert-message', this);
         $(".form").on("submit", function () {
             var check = $('.check', this),
                 message = $('.alert-message', this),
                 reNone = /.+/,
-                email = $('.input-mail', this),
-                button = $('.button-form', this);
+                email = $('.input-mail', this);
             if (!email.val().match(reNone)) {
-                message.text('Введите email').slideDown(500);
+                message.text('↑ Введите email').slideDown(500);
                 return false;
             }
             if (!check.prop("checked")) {
-                check.next().css({
-                    'color': 'red',
-                    'transition': 'all .4s ease'
-                });
-                check.next().children().css({
-                    'color': 'red',
-                    'transition': 'all .4s ease'
-                });
-                message.text('Подтвердите соглашение').slideDown(500);
+                message.text('↙ ✔Подтвердите соглашение').slideDown(500);
                 return false;
             }
             if (email.val() && check) {
-
-                // button.text('Отправляем...');
-                // setTimeout(function () {
-                //     button.text('Отправлено!');
-                // }, 500);
                 return true
             }
         });
         email.click(function () {
-            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
             message.slideUp(500);
         });
         check.click(function () {
-            check.next().css({
-                "color": "#000",
-                'transition': 'all .4s ease'
-            });
-            check.next().children().css({
-                "color": "#000",
-                'transition': 'all .4s ease'
-            });
             message.slideUp(500);
         });
     });
