7c26dac05a077f87d842ca6147f6a8fb51b321f4
PavelRoR <pavelvp2182@yandex.ru>
2021-09-06 12:11:23 +0500

    Revs edits

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -33,10 +33,10 @@ $(document).ready(function () {
     $(".video-wrapper-rev").click(function () {
         $(".video-wrapper-rev iframe").each(function () {
             var l = $(this).parent().attr('data-img');
-            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв"><div class="yt-button"></div>');
         })
         var a = $(this).attr("data-youtube");
-        $(this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1"  allowfullscreen></iframe>')
+        $(this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1" allowfullscreen></iframe>')
     });
     /*Конец документа*/
 });
